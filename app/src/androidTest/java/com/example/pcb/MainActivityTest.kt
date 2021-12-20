4b03f26451d90e751e7c51dcf6d35997433fe9ca
Kamil Jackiewicz <kamil.jackiewicz13@gmail.com>
2021-12-20 17:14:12 +0100

    Add test checking CPU button after using switch

--- app/src/androidTest/java/com/example/pcb/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/pcb/MainActivityTest.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -24,14 +25,17 @@ class MainActivityTest {
         scenario = launchActivity()
         scenario.moveToState(Lifecycle.State.RESUMED)
     }
-
+@Test
+fun test_checkIfMainActivityStarted(){
+    onView(withId(R.id.main_view)).check(matches(isDisplayed()))
+}
     @Test
-    fun checkMainActivityButton() {
+    fun test_checkMainActivityButton() {
         onView(withId(R.id.start_button)).perform(click())
     }
 
     @Test
-    fun checkSecondActivityButtons() {
+    fun test_checkSecondActivityButtons() {
         onView(withId(R.id.start_button)).perform(click())
         onView(withId(R.id.mobo_button)).check(matches(isDisplayed()))
         onView(withId(R.id.psu_button)).check(matches(isDisplayed()))
@@ -42,5 +46,27 @@ class MainActivityTest {
         onView(withId(R.id.cpu_button)).check(matches(isDisplayed()))
         onView(withId(R.id.gpu_button)).check(matches(isDisplayed()))
         onView(withId(R.id.switch_intel_amd)).check(matches(isDisplayed()))
+    }
+    @Test
+    fun test_BackPress_ToMainActivity(){
+        onView(withId(R.id.start_button)).perform(click())
+        pressBack()
+        onView(withId(R.id.main_view)).check(matches(isDisplayed()))
+    }
+    @Test
+    fun test_checkSwitchButton(){
+        onView(withId(R.id.start_button)).perform(click())
+        onView(withId(R.id.switch_intel_amd)).perform(click())
+    }
+    @Test
+    fun test_checkCPUButtonAfterSwitch(){
+        onView(withId(R.id.start_button)).perform(click())
+        onView(withId(R.id.cpu_button)).perform(click())
+        onView(withId(R.id.cpuIntelActivity)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.switch_intel_amd)).perform(click())
+        onView(withId(R.id.cpu_button)).perform(click())
+        onView(withId(R.id.cpuAMDActivity)).check(matches(isDisplayed()))
+        pressBack()
     }
 }
